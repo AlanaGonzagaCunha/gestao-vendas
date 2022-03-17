412b4b5a9b666e15c7d6efabb71418fddde0c086
Alana Gonzaga <alanagonzagac@gmail.com>
2022-03-17 20:09:08 -0300

    Creating Product Service

--- gestao-vendas/src/main/java/br/com/gvendas/respositorio/ProdutoRepositorio.java
+++ gestao-vendas/src/main/java/br/com/gvendas/respositorio/ProdutoRepositorio.java
@@ -1,5 +1,27 @@
 package br.com.gvendas.respositorio;
 
-public class ProdutoRepositorio {
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import br.com.gvendas.entidades.Produto;
+
+@Repository
+public interface ProdutoRepositorio extends JpaRepository <Produto, Long>{
+	
+	List <Produto> findByCategoriaCodigo(Long codigoCategoria);
+	
+	@Query("Select prod"
+			+ " from Produto prod"
+			+ " where prod.codigo = :codigo"
+			+ "   and prod.categoria.codigo = :codigoCategoria")
+	Optional <Produto> buscarPorCodigo(Long codigo, Long codigoCategoria);
+	
+	Optional <Produto> findByCategoriaCodigoAndDescricao(Long codigoCategoria, String descricao);
+	
+	void deleteByCodigoAndCategoriaCodigo(Long codigoProduto, Long codigoCategoria);
 }
--- gestao-vendas/src/main/java/br/com/gvendas/servico/ProdutoServico.java
+++ gestao-vendas/src/main/java/br/com/gvendas/servico/ProdutoServico.java
@@ -1,5 +1,31 @@
 package br.com.gvendas.servico;
 
-public class ProdutoServico {
+import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import br.com.gvendas.entidades.Produto;
+import br.com.gvendas.respositorio.ProdutoRepositorio;
+
+@Service
+public class ProdutoServico {
+	
+	@Autowired
+	private ProdutoRepositorio produtoRepositorio;
+	
+	public List <Produto> listarTodos() {
+		return produtoRepositorio.findAll();
+	}
+
+	public Optional<Produto> listaPorCodigo (@PathVariable Long codigoProduto, @PathVariable Long codigoCategoria  ) {
+		return produtoRepositorio.buscarPorCodigo(codigoProduto, codigoCategoria); 
+	}
+	
+	public void salvar() {
+		
+	}
+	
 }
